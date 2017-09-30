eeff07e4435e64acb575b7cd206cb7842db3b44f
Igor Inkovskiy <Inkovskiy@gmail.com>
2017-09-30 11:51:28 +0300

    test(Page Analyzer) testing with DB

--- tests/DomainsTest.php
+++ tests/DomainsTest.php
@@ -4,46 +4,12 @@ namespace UnitTests;
 
 use Illuminate\Support\Facades\Artisan;
 
-//use Illuminate\Database\DatabaseManager;
-
-//use Laravel\Lumen\Testing\DatabaseMigrations;
-
 use Illuminate\Support\Facades\DB;
 
 use Carbon\Carbon;
 
 class DomainsTest extends TestCase
 {
-
-//    use DatabaseMigrations;
-
-    /**
-     * A basic response test.
-     *
-     * @return void
-     */
-//    public function testApplication()
-//    {
-//        $response = $this->call('GET', 'domains');
-//        $this->assertEquals(200, $response->status());
-//    }
-
-//    TODO: чтобы он использовал route('storeDomain', [['url' => 'http://testdatabase.com']]);
-    public function testDatabase()
-    {
-        echo '3Путин - хуй!';
-        $f = DB::table('migrations')->first();
-        print_r($f);
-        $id = DB::table('domains')->insertGetId(
-            [
-                'name' => 'http://test.com',
-                'created_at' => Carbon::now()
-            ]
-        );
-        echo "YES bitch: {$id} %))))))";
-        $this->seeInDatabase('domains', ['id' => $id]);
-    }
-
     public function setUp()
     {
         putenv('DB_CONNECTION=testing');
@@ -53,8 +19,37 @@ class DomainsTest extends TestCase
 
     public function tearDown()
     {
-//        putenv('DB_CONNECTION=testing');
         Artisan::call('migrate:reset');
         parent::tearDown();
+    }
+
+    /**
+     * A basic response test.
+     *
+     * @return void
+     */
+    public function testApplication()
+    {
+        $id = DB::table('domains')->insertGetId(
+            [
+                'name' => 'http://test.com',
+                'created_at' => Carbon::now()
+            ]
+        );
+        $response = $this->call('GET', 'domains');
+        $this->assertEquals(200, $response->status());
+    }
+
+//    TODO: чтобы он использовал route('storeDomain', [['url' => 'http://testdatabase.com']]);
+    public function testDatabase()
+    {
+        $id = DB::table('domains')->insertGetId(
+            [
+                'name' => 'http://test.com',
+                'created_at' => Carbon::now()
+            ]
+        );
+        echo "YES bitch: {$id} %))))))";
+        $this->seeInDatabase('domains', ['id' => $id]);
     }
 }
